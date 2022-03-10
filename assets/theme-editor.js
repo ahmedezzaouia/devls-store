4d74e1395c7ee899eeed88b38200b8c42a435a3d
Ahmed Ez zaouia <ahmez-za@e1r9p12.1337.ma>
2022-03-10 13:00:54 +0100

    fix scroll

--- assets/theme-editor.js
+++ assets/theme-editor.js
@@ -18,3 +18,6 @@ document.addEventListener('shopify:block:deselect', function(event) {
   const parentSlideshowComponent = event.target.closest('slideshow-component');
   if (parentSlideshowComponent.autoplayButtonIsSetToPlay) parentSlideshowComponent.play();
 });
+
+
+// **************** Custom Code ****************
